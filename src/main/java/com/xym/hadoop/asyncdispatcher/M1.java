d4b6bf398a25c592a02953e1bf430621bbe008c8
YIMINGXU <100650920@qq.com>
2015-11-06 18:09:34 +0800

    状态机详解

--- src/main/java/com/xym/hadoop/asyncdispatcher/M1.java
+++ src/main/java/com/xym/hadoop/asyncdispatcher/M1.java
@@ -12,40 +12,40 @@ import org.apache.hadoop.yarn.state.SingleArcTransition;
 import org.apache.hadoop.yarn.state.StateMachine;
 import org.apache.hadoop.yarn.state.StateMachineFactory;
 /**
- * 这是一个状态机,这个状态机用来控制jobevent的整个运行流程
+ * 这是一个状态机,这个状态机用来控制job1的整个运行流程
  * @author imad
  *
  */
-public class JobStateMachine implements EventHandler<JobEvent> {
+public class M1 implements EventHandler<job1> {
 	// private final String jobId;
-	private EventHandler<JobEvent> eventHandler;
+	private EventHandler eventHandler;
 	private final Lock writeLock;
 	private final Lock readLock;
-	static StateMachine<JobStateInternal, JobEventType, JobEvent> stateMachine;
+	static StateMachine<JobStateInternal, Event1, job1> stateMachine;
 	/**
 	 * 构建一个状态机的DAG   这个DAG中分别分开始状态 结束状态  job事件
 	 */
-	protected static final StateMachineFactory<JobStateMachine, JobStateInternal, JobEventType, JobEvent> STATE_MACHINE_FACTORY = new StateMachineFactory<JobStateMachine, JobStateInternal, JobEventType, JobEvent>(
+	protected static final StateMachineFactory<M1, JobStateInternal, Event1, job1> STATE_MACHINE_FACTORY = new StateMachineFactory<M1, JobStateInternal, Event1, job1>(
 			JobStateInternal.NEW)
 			//忽略的事件  这个是源码中没引用  但是没删除的代码
 //			.addTransition(JobStateInternal.CREATE, JobStateInternal.NEW,
-//					JobEventType.JOB_PREPARE)
+//					Event1.JOB_PREPARE)
 			//这里 的job_INIT任务事件同时用了2次, 可以体现状态机的细分任务的作用
 			.addTransition(JobStateInternal.NEW, JobStateInternal.INITED,
-					JobEventType.JOB_INIT, new InitTransition())
+					Event1.JOB_INIT, new InitTransition())
 			.addTransition(JobStateInternal.INITED, JobStateInternal.SETUP,
-					JobEventType.JOB_INIT, new StartInitTransition())
+					Event1.JOB_INIT, new StartInitTransition())
 			.addTransition(JobStateInternal.SETUP, JobStateInternal.RUNNING,
-					JobEventType.JOB_START, new StartTransition())
+					Event1.JOB_START, new StartTransition())
 			.addTransition(JobStateInternal.RUNNING, JobStateInternal.SUCCEEDED,
-					JobEventType.JOB_SETUP_COMPLETED,
+					Event1.JOB_SETUP_COMPLETED,
 					new SetupCompletedTransition())
 			.addTransition(
 					JobStateInternal.SUCCEEDED,
 					//如果使用enumset的话,需要继承的MultipleArcTransition  返回值就是类型
 					EnumSet.of(JobStateInternal.KILLED,
 							JobStateInternal.SUCCEEDED),
-					JobEventType.JOB_COMPLETED,
+					Event1.JOB_COMPLETED,
 					new JobTasksCompletedTransition()).installTopology();
 
 	// public JobStateMachine(String jobId, EventHandler eventHandler) {
@@ -56,7 +56,7 @@ public class JobStateMachine implements EventHandler<JobEvent> {
 	// this.eventHandler = eventHandler;
 	// this.stateMachine = STATE_MACHINE_FACTORY.make(this);
 	// }
-	public JobStateMachine(EventHandler eventHandler) {
+	public M1(EventHandler eventHandler) {
 		ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 		this.readLock = readWriteLock.readLock();
 		this.writeLock = readWriteLock.writeLock();
@@ -64,18 +64,16 @@ public class JobStateMachine implements EventHandler<JobEvent> {
 		this.stateMachine = STATE_MACHINE_FACTORY.make(this);
 	}
 
-	protected static StateMachine<JobStateInternal, JobEventType, JobEvent> getStateMachine() {
+	protected static StateMachine<JobStateInternal, Event1, job1> getStateMachine() {
 		return stateMachine;
 	}	
 	/**
 	 * 调用handle方法 转换每一个事件 一次调用  
 	 */
-	public void handle(JobEvent event) {
+	public void handle(job1 event) {
 		try {
 			writeLock.lock();
 			JobStateInternal oldState = getInternalState();
-			System.out.println("the oldstate is    " + oldState
-					+ "   ..........");
 			try {
 				getStateMachine().doTransition(event.getType(), event);
 			} catch (InvalidStateTransitonException e) {
@@ -101,51 +99,52 @@ public class JobStateMachine implements EventHandler<JobEvent> {
 	}
 
 	static private class InitTransition implements
-			SingleArcTransition<JobStateMachine, JobEvent> {
+			SingleArcTransition<M1, job1> {
 
-		public void transition(JobStateMachine operand, JobEvent event) {
-			System.out.println("job 1 .......");
-			operand.eventHandler.handle(new JobEvent(event.getJobId(),
-					JobEventType.JOB_INIT));
+		public void transition(M1 operand, job1 event) {
+			System.out.println("job 1 1.......");
+			operand.eventHandler.handle(new job1(event.getJobId(),
+					Event1.JOB_INIT));
 		}
 	}
 
 	static private class StartInitTransition implements
-			SingleArcTransition<JobStateMachine, JobEvent> {
+			SingleArcTransition<M1, job1> {
 
-		public void transition(JobStateMachine operand, JobEvent event) {
-			System.out.println("job   2 ......");
-//			operand.eventHandler.handle(new JobEvent(event.getJobId(),
-//					JobEventType.JOB_START));
+		public void transition(M1 operand, job1 event) {
+			System.out.println("job  1 2 ...... 切到 M2 ");
+			operand.eventHandler.handle(new job2(event.getJobId(), Event2.JOB_INIT));
 		}
 	}
 
 	static private class StartTransition implements
-			SingleArcTransition<JobStateMachine, JobEvent> {
+			SingleArcTransition<M1, job1> {
 
-		public void transition(JobStateMachine operand, JobEvent event) {
-			System.out.println("job 3 .......");
-			operand.eventHandler.handle(new JobEvent(event.getJobId(),
-					JobEventType.JOB_SETUP_COMPLETED));
+		public void transition(M1 operand, job1 event) {
+			System.out.println("job 1 3 .......");
+			operand.eventHandler.handle(new job1(event.getJobId(),
+					Event1.JOB_SETUP_COMPLETED));
 		}
 	}
 
 	static private class SetupCompletedTransition implements
-			SingleArcTransition<JobStateMachine, JobEvent> {
+			SingleArcTransition<M1, job1> {
 
-		public void transition(JobStateMachine operand, JobEvent event) {
-			System.out.println("job 4 .......");
-			operand.eventHandler.handle(new JobEvent(event.getJobId(),
-					JobEventType.JOB_COMPLETED));
+		public void transition(M1 operand, job1 event) {
+			System.out.println("job 1  4 .......");
+			operand.eventHandler.handle(new job1(event.getJobId(),
+					Event1.JOB_COMPLETED));
 		}
 	}
 
 	static public class JobTasksCompletedTransition implements
-			MultipleArcTransition<JobStateMachine, JobEvent, JobStateInternal> {
+			MultipleArcTransition<M1, job1, JobStateInternal> {
 
-		public JobStateInternal transition(JobStateMachine operand,
-				JobEvent event) {
-			System.out.println("job 5 .......");
+		public JobStateInternal transition(M1 operand,
+				job1 event) {
+			System.out.println("job 1 5 .......继续切M2");
+			operand.eventHandler.handle(new job2("job_111", Event2.JOB_START));
+
 			return null;
 		}
 
--- src/main/java/com/xym/hadoop/asyncdispatcher/M2.java
+++ src/main/java/com/xym/hadoop/asyncdispatcher/M2.java
@@ -12,40 +12,40 @@ import org.apache.hadoop.yarn.state.SingleArcTransition;
 import org.apache.hadoop.yarn.state.StateMachine;
 import org.apache.hadoop.yarn.state.StateMachineFactory;
 /**
- * 这是一个状态机,这个状态机用来控制jobevent的整个运行流程
+ * 这是一个状态机,这个状态机用来控制job2的整个运行流程
  * @author imad
  *
  */
-public class JobStateMachine implements EventHandler<JobEvent> {
+public class M2 implements EventHandler<job2> {
 	// private final String jobId;
-	private EventHandler<JobEvent> eventHandler;
+	private EventHandler eventHandler;
 	private final Lock writeLock;
 	private final Lock readLock;
-	static StateMachine<JobStateInternal, JobEventType, JobEvent> stateMachine;
+	static StateMachine<JobStateInternal, Event2, job2> stateMachine;
 	/**
 	 * 构建一个状态机的DAG   这个DAG中分别分开始状态 结束状态  job事件
 	 */
-	protected static final StateMachineFactory<JobStateMachine, JobStateInternal, JobEventType, JobEvent> STATE_MACHINE_FACTORY = new StateMachineFactory<JobStateMachine, JobStateInternal, JobEventType, JobEvent>(
+	protected static final StateMachineFactory<M2, JobStateInternal, Event2, job2> STATE_MACHINE_FACTORY = new StateMachineFactory<M2, JobStateInternal, Event2, job2>(
 			JobStateInternal.NEW)
 			//忽略的事件  这个是源码中没引用  但是没删除的代码
 //			.addTransition(JobStateInternal.CREATE, JobStateInternal.NEW,
-//					JobEventType.JOB_PREPARE)
+//					Event2.JOB_PREPARE)
 			//这里 的job_INIT任务事件同时用了2次, 可以体现状态机的细分任务的作用
 			.addTransition(JobStateInternal.NEW, JobStateInternal.INITED,
-					JobEventType.JOB_INIT, new InitTransition())
+					Event2.JOB_INIT, new InitTransition())
 			.addTransition(JobStateInternal.INITED, JobStateInternal.SETUP,
-					JobEventType.JOB_INIT, new StartInitTransition())
+					Event2.JOB_INIT, new StartInitTransition())
 			.addTransition(JobStateInternal.SETUP, JobStateInternal.RUNNING,
-					JobEventType.JOB_START, new StartTransition())
+					Event2.JOB_START, new StartTransition())
 			.addTransition(JobStateInternal.RUNNING, JobStateInternal.SUCCEEDED,
-					JobEventType.JOB_SETUP_COMPLETED,
+					Event2.JOB_SETUP_COMPLETED,
 					new SetupCompletedTransition())
 			.addTransition(
 					JobStateInternal.SUCCEEDED,
 					//如果使用enumset的话,需要继承的MultipleArcTransition  返回值就是类型
 					EnumSet.of(JobStateInternal.KILLED,
 							JobStateInternal.SUCCEEDED),
-					JobEventType.JOB_COMPLETED,
+					Event2.JOB_COMPLETED,
 					new JobTasksCompletedTransition()).installTopology();
 
 	// public JobStateMachine(String jobId, EventHandler eventHandler) {
@@ -56,7 +56,7 @@ public class JobStateMachine implements EventHandler<JobEvent> {
 	// this.eventHandler = eventHandler;
 	// this.stateMachine = STATE_MACHINE_FACTORY.make(this);
 	// }
-	public JobStateMachine(EventHandler eventHandler) {
+	public M2(EventHandler eventHandler) {
 		ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 		this.readLock = readWriteLock.readLock();
 		this.writeLock = readWriteLock.writeLock();
@@ -64,18 +64,16 @@ public class JobStateMachine implements EventHandler<JobEvent> {
 		this.stateMachine = STATE_MACHINE_FACTORY.make(this);
 	}
 
-	protected static StateMachine<JobStateInternal, JobEventType, JobEvent> getStateMachine() {
+	protected static StateMachine<JobStateInternal, Event2, job2> getStateMachine() {
 		return stateMachine;
 	}	
 	/**
 	 * 调用handle方法 转换每一个事件 一次调用  
 	 */
-	public void handle(JobEvent event) {
+	public void handle(job2 event) {
 		try {
 			writeLock.lock();
 			JobStateInternal oldState = getInternalState();
-			System.out.println("the oldstate is    " + oldState
-					+ "   ..........");
 			try {
 				getStateMachine().doTransition(event.getType(), event);
 			} catch (InvalidStateTransitonException e) {
@@ -101,51 +99,51 @@ public class JobStateMachine implements EventHandler<JobEvent> {
 	}
 
 	static private class InitTransition implements
-			SingleArcTransition<JobStateMachine, JobEvent> {
+			SingleArcTransition<M2, job2> {
 
-		public void transition(JobStateMachine operand, JobEvent event) {
-			System.out.println("job 1 .......");
-			operand.eventHandler.handle(new JobEvent(event.getJobId(),
-					JobEventType.JOB_INIT));
+		public void transition(M2 operand, job2 event) {
+			System.out.println("job 2 1 .......");
+			operand.eventHandler.handle(new job2(event.getJobId(),
+					Event2.JOB_INIT));
 		}
 	}
 
 	static private class StartInitTransition implements
-			SingleArcTransition<JobStateMachine, JobEvent> {
+			SingleArcTransition<M2, job2> {
 
-		public void transition(JobStateMachine operand, JobEvent event) {
-			System.out.println("job   2 ......");
-//			operand.eventHandler.handle(new JobEvent(event.getJobId(),
-//					JobEventType.JOB_START));
+		public void transition(M2 operand, job2 event) {
+			System.out.println("job  2 2 ......切到M1");
+			operand.eventHandler.handle(new job1("job_111", Event1.JOB_START));
+			
 		}
 	}
 
 	static private class StartTransition implements
-			SingleArcTransition<JobStateMachine, JobEvent> {
+			SingleArcTransition<M2, job2> {
 
-		public void transition(JobStateMachine operand, JobEvent event) {
-			System.out.println("job 3 .......");
-			operand.eventHandler.handle(new JobEvent(event.getJobId(),
-					JobEventType.JOB_SETUP_COMPLETED));
+		public void transition(M2 operand, job2 event) {
+			System.out.println("job 2 3 .......");
+			operand.eventHandler.handle(new job2(event.getJobId(),
+					Event2.JOB_SETUP_COMPLETED));
 		}
 	}
 
 	static private class SetupCompletedTransition implements
-			SingleArcTransition<JobStateMachine, JobEvent> {
+			SingleArcTransition<M2, job2> {
 
-		public void transition(JobStateMachine operand, JobEvent event) {
-			System.out.println("job 4 .......");
-			operand.eventHandler.handle(new JobEvent(event.getJobId(),
-					JobEventType.JOB_COMPLETED));
+		public void transition(M2 operand, job2 event) {
+			System.out.println("job 2  4 .......");
+			operand.eventHandler.handle(new job2(event.getJobId(),
+					Event2.JOB_COMPLETED));
 		}
 	}
 
 	static public class JobTasksCompletedTransition implements
-			MultipleArcTransition<JobStateMachine, JobEvent, JobStateInternal> {
+			MultipleArcTransition<M2, job2, JobStateInternal> {
 
-		public JobStateInternal transition(JobStateMachine operand,
-				JobEvent event) {
-			System.out.println("job 5 .......");
+		public JobStateInternal transition(M2 operand,
+				job2 event) {
+			System.out.println("job 2 5 .......");
 			return null;
 		}
 
